78bfb5c182db6be0f5fb3be0df694d2baac78756
@gabrielaguirre004 <97920969+gabrielaguirre004@users.noreply.github.com>
2022-03-24 17:35:26 -0300

    boletines
    inclui a este repositorio la carpeta boletines que es una tabla de clasificacion de escuelas privadas

---
+++ boletines/JavaScript/main.js
@@ -0,0 +1,61 @@
+/*console.log("Hola desde un archivo externo de JavaScript");
+//DOM
+//querySelector /querySelectorAll
+let links=document.querySelectorAll("a");
+console.log(links);
+let links = document.querySelectorAll("a");
+links.forEach {function (links)
+console.log(links);
+} } 
+let cedas = document.querySelectorAll("td");
+celdas.forEach(function(td){
+td.addEventListener("click",function(){
+   console.log(this);
+})
+});
+
+// obtener los elementos de la clase .close
+ let links= document.querySelectorAll(".close");
+ // recorrerlos
+ links.forEach(function (link) {
+    // agregar un evento click a cada uno de ellos 
+    links.addEventListener("click",function(ev) {
+      ev.preventDefault();
+      return false;    
+    });
+ });
+
+ let iconos=document.querySelectorAll("i");
+   icono.classList.remove("fa-star-o");
+   icono.classList.add("fa-star");
+   {)
+   */
+  // obtener los elementos de la clase .close
+ let links= document.querySelectorAll(".close");
+ // recorrerlos
+ links.forEach(function (link){ 
+
+    // agregar un evento click a cada uno de ellos 
+          links.addEventListener("click",function(ev){
+           ev.preventDefault();
+          let contet = document.querySelector(".content");
+// quitarle las clases de animacion que ya tiene
+            content.classList.remove("fadeindown");
+            content.classList.remove("animated");
+// agregar clases para animar su salida fadeoutup-      
+            content.classList.add("fadeoutup");
+           content.classList.add("animated");
+
+          setTimeout(function(){
+
+           location.href ="/boletines"; 
+           },2000);
+
+            retun false;
+   });
+    });       
+         
+
+
+
+
